fb7992a9e53f66c2e685ea5b3c2cb843d2e06dd2
Chris Nearing <clnearing@gmail.com>
2024-05-27 13:41:08 +0700

    rails yelp mvp

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,38 @@
+class RestaurantsController < ApplicationController
+  # GET "restaurants"
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  # GET "restaurants/new"
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new, status: :unprocessable_entity
+    end
+
+  end
+
+
+  # GET "restaurants/38"
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  # POST "restaurants"
+  def add
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+  end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'faker'
 # This file should ensure the existence of records required to run the application in every environment (production,
 # development, test). The code here should be idempotent so that it can be executed at any point in every environment.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
@@ -7,3 +8,22 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+# 5.times do
+#   Restaurant.create(
+#     name: Faker::Restaurant.name,
+#     address: Faker::Address.full_address,
+#     phone_number: Faker::PhoneNumber.phone_number,
+#     category: ["chinese", "italian", "japanese", "french", "belgian"].sample
+#   )
+# end
+
+# 5.times do
+#   rests = Restaurant.all
+#   rests.each do |rest|
+#     p review = Review.new
+#     p review.content = Faker::Restaurant.review
+#     p review.rating = [0, 1, 2, 3, 4, 5].sample
+#     p review.restaurant_id = rest.id
+#     review.save
+#   end
+# end
